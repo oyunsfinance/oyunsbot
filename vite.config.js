34d87f1f4491d2c64918ac2a73b309c02788ab38
oyunsfinance <anujin4x@gmail.com>
2026-03-04 20:31:43 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/oyuns-finance-app/',  // GitHub repo нэрээ солино
+  base: '/oyunsbot/',  // GitHub repo нэрээ солино
   build: {
     outDir: 'dist',
     sourcemap: false,
